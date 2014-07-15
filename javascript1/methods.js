d8cfa72c2f9c0a029cd7a5d725a9ec148ca080dd
kate <kate_evstratenko@mail.ru>
2014-07-15 17:22:25 +0300

    added methods

---
+++ javascript1/methods.js
@@ -0,0 +1,28 @@
+﻿function GetResultValue(a, b, flag) {
+    console.clear();
+
+
+    var c = a * flag + b * !flag;
+    console.log(Number(c));
+
+
+    var c = Number(flag && a) + Number(!flag && b);
+    console.log(c);
+    
+
+    var c = (flag || b) * (!flag || a);
+    console.log(Number(c));
+
+
+    var c = (!flag && b) || (flag && a);
+    console.log(Number(c));
+
+
+    var c = (isNaN(0 / flag) * b) + (!isNaN(0 / flag) * a);
+    console.log(Number(c));
+
+
+    var c = (flag.toString() == 'true') * a + (flag.toString() == 'false') * b;
+    console.log(Number(c));
+};
+
---
+++ javascript1/program.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var a = document.getElementById('aField');
+    var b = document.getElementById('bField');
+    var flag = document.getElementById('flag');
+
+
+    var $form = document.getElementById('inputForm');
+    $form.addEventListener('click', function (e) {
+        if (e.target.type == 'button') {
+            GetResultValue(a.value, b.value, flag.checked);
+        }
+    }, false);
+    
+})();
+
